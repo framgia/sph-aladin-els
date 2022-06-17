99fe4a68f7a6e81acdec8286e816edecd38a62d7
Aladin <aladin.penagunda@sun-asterisk.com>
2022-06-17 17:03:57 +0800

    Create an api that will send over all of the user and their info

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,57 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Center,
+  Alert,
+  AlertIcon,
+  Flex,
+  AlertTitle,
+} from "@chakra-ui/react";
+
+import "./App.css";
+
+import SignupPage from "./pages/SignUp";
+import { userSelect } from "./redux/userSlice";
+import { useAppSelector } from "./redux/hooks";
+import { BrowserRouter, Link } from "react-router-dom";
+import Routers from "./routes/Routers";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
+  const { message, messageType }: any = useAppSelector(userSelect);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      {message && message.length > 0 && (
+        <Alert status={messageType}>
+          <AlertIcon />
+          <AlertTitle>{message}</AlertTitle>
+        </Alert>
+      )}
+      <Flex>
+        <Link to="user-list">User List</Link>
+      </Flex>
+      <Center h="980px">
+        <Container maxW="md" h={"400px"}>
+          <Routers />
+        </Container>
+      </Center>
+    </BrowserRouter>
   );
 }
 
---
+++ fe/src/routes/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import useAuth from "../utils/useAuth";
+
+export default function PrivateRoute({ children }: any) {
+  const auth = useSelector((state: RootState) => state.user.isSignedIn);
+  return auth ? children : <Navigate to="/login" />;
+}
