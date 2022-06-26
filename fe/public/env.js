f6b10a03f33ae8ada89ddc1163a908bcfc243e83
Aladin <aladin.penagunda@sun-asterisk.com>
2022-06-26 19:53:10 +0800

    fix merging error

--- fe/public/env.js
+++ fe/public/env.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 window.env = {
-  "BASE_URL": "http://localhost:3000/"
+  BASE_URL: "http://localhost:3000/",
 };
-=======
-window.env = {};
->>>>>>> 1738d8b (Fix/file structure (#12))
