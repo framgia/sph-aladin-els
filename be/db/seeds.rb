e2853b8250eb077e652b5c0d09fb835500d4f846
Aladin <aladin.penagunda@sun-asterisk.com>
2022-06-29 09:34:28 +0800

    [Markup] Create a page showing all of the quizzes and action buttons

--- be/db/seeds.rb
+++ be/db/seeds.rb
@@ -10,6 +10,7 @@
 Quiz.create(title: "English 1", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
 Quiz.create(title: "Spanish", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
 Quiz.create(title: "Cantonese", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
+Quiz.create(title: "Cantonese", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
 
 #  Admin user
 
